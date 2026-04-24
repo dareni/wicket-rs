995fa09e92a6fd644926322e7f71c7ad73298cac
Daren Isaacs <ikkyisaacs@gmail.com>
2026-04-24 12:43:46 +1000

    feat: redefine Page, WebPage
    Swap the roles, WebPage is now the trait.
    New render_component() logic.

--- crates/wicket-core/src/components.rs
+++ crates/wicket-core/src/components.rs
@@ -1,28 +1,40 @@
-use std::fmt::Display;
+use std::{collections::HashMap, fmt::Display};
 
 use crate::request::Response;
 
 pub trait Component {
     fn markup_id(&self) -> &str;
     fn set_internal_id(&self, id: InternalId);
+    fn render(&self, response: &Response);
 }
 pub struct MarkupContainer {}
 
 #[derive(Default)]
 pub struct MarkupIdGenerator {}
 
-pub trait Page {
-    fn render_component(&self, _id: InternalId, _response: &Response) {
-        todo!()
-    }
+pub enum ComponentId {
+    Internal(InternalId),
+    TagId(u16),
 }
 
-pub struct WebPage {
+pub trait WebPage {
+    ///Render the component from ajax context
+    fn render_component(&self, id: ComponentId, response: &mut Response);
+}
+
+pub struct Page {
+    //Internal component id, should this be the index into components?.
     id_counter: u16,
+    //Unique Id for this page instance.
+    _instance_id: u8,
+    //central page component store
     components: Vec<Box<dyn Component>>,
+    tag_id_map: HashMap<u16, InternalId>,
+    //direct children
+    children: Vec<u16>,
 }
 
-impl WebPage {
+impl Page {
     fn next_id(&mut self) -> InternalId {
         let id = InternalId::from(self.id_counter);
         // Safety check for u16 overflow
@@ -33,17 +45,52 @@ impl WebPage {
         id
     }
 
-    pub fn add(&mut self, component: Box<dyn Component>) -> InternalId {
+    pub fn store(&mut self, component: Box<dyn Component>) -> InternalId {
         let id = self.next_id();
         component.set_internal_id(id);
         self.components.insert(id.into(), component);
         id
     }
+
+    pub fn add(&mut self, component: Box<dyn Component>) {
+        let id = self.store(component);
+        self.children.push(id.into());
+    }
 }
 
+impl WebPage for Page {
+    fn render_component(&self, id: ComponentId, response: &mut Response) {
+        let component_id: u16 = match id {
+            ComponentId::Internal(internal) => internal.into(),
+            ComponentId::TagId(id) => {
+                let internal_id = self
+                    .tag_id_map
+                    .get(&id)
+                    .unwrap_or_else(|| panic!("No mapping to InternalId for tagid:{}", id));
+                (*internal_id).into()
+            }
+        };
+        let component = self
+            .components
+            .get(component_id as usize)
+            .unwrap_or_else(|| {
+                panic!(
+                    "Component id:{} does not exist in page cache.",
+                    component_id
+                )
+            });
+        component.render(response);
+    }
+}
 /// A type-safe wrapper for component IDs within a single Page.
 #[derive(Debug, Default, Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord)]
 pub struct InternalId(pub(crate) u16);
+
+impl From<InternalId> for u16 {
+    fn from(value: InternalId) -> Self {
+        value.0
+    }
+}
 
 impl From<u16> for InternalId {
     #[inline]
